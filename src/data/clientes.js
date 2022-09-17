3fdd95606323deb3723af0acacf271dcf3f99e0a
jorge diaz <jorge.diaz3887@gmail.com>
2022-09-17 18:41:11 -0300

    datos de prueba

---
+++ src/data/clientes.js
@@ -0,0 +1,57 @@
+export default clientes = [
+    {
+        id: 1,
+        nombre: "Juan",
+        apellido: "Mendez",
+        dni: "44565678",
+        telefono: 3884908501,
+        email: "juan.m@gmail.com",
+        direccion: "Necochea N°1100",
+        participaSorteo: false,
+        participaPromocion: true
+    },
+    {
+        id: 2,
+        nombre: "Lucas",
+        apellido: "Mamani",
+        dni: "35907506",
+        telefono: 3885287872,
+        email: "lucas.mamani1@gmail.com",
+        direccion: "Las Vicuñas 206",
+        participaSorteo: true,
+        participaPromocion: true
+    },
+    {
+        id: 3,
+        nombre: "Fatima",
+        apellido: "Herrera",
+        dni: "37450687",
+        telefono: 3884716899,
+        email: "fati.herrera@gmail.com",
+        direccion: "Aleli N°304",
+        participaSorteo: false,
+        participaPromocion: false
+    },
+    {
+        id: 4,
+        nombre: "Dominga",
+        apellido: "Rueda",
+        dni: "20807654",
+        telefono: 3884876267,
+        email: "dominga.rueda01@gmail.com",
+        direccion: "Av. Fascio 1010",
+        participaSorteo: true,
+        participaPromocion: true
+    },
+    {
+        id: 5,
+        nombre: "Maximiliano",
+        apellido: "Agustini",
+        dni: "40656788",
+        telefono: 3884987387,
+        email: "maxiagustini@gmail.com",
+        direccion: "19 de Abril",
+        participaSorteo: true,
+        participaPromocion: true
+    },
+]
